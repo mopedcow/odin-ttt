7b7c79a529847730de8c6f30ef2bc2f0c6e9aafb
mopedcow <188090362+mopedcow@users.noreply.github.com>
2025-08-06 21:15:32 +0100

    Create 'checkForWin' function

--- script.js
+++ script.js
@@ -2,20 +2,28 @@
 
 
 const gameboard = (function () {
-    let row1 = [ , , ,];
-    let row2 = [ , , ,];
-    let row3 = [ , , ,];
-    return {row1, row2, row3};
+    let row0 = ['n', 'n', 'n'];
+    let row1 = ['n', 'n', 'n'];
+    let row2 = ['n', 'n', 'n'];
+    return {row0, row1, row2};
 });
 
 function createPlayer (name, marker) {
     return { name, marker };
 }
 
-function placeMarker(marker, row, col) {
+function placeMarker (marker, row, col) {
     row = `row${row}`;
     currGame[row].splice(col, 1, marker);
     console.log(currGame);
+}
+
+function checkForWin (marker) {
+    if (currGame.row0[0] == marker &&
+        currGame.row1[0] == marker &&
+        currGame.row2[0] == marker) {
+            return true;
+        }
 }
 
 const player1 = createPlayer('Wallace', 'x');
