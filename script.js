c1b6cc1e00d8f05b98f561722deea6aea604059f
mopedcow <188090362+mopedcow@users.noreply.github.com>
2025-08-06 16:24:33 +0100

    Create 'gameboard' object

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('hello');
+const title = document.getElementById('title');
+title.innerHTML = 'Console game!!';
+/* remove above code later */
+
+
+
+const gameboard = (function () {
+    let row1 = [ , , ,];
+    let row2 = [ , , ,];
+    let row3 = [ , , ,];
+    return {row1, row2, row3};
+});
+
+const players = {
+    
+};
+
+const gameplay = {};
+
+let currGame = gameboard();
