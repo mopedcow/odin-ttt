ef2471ff719a518c18f05a673dd710e87a65b424
mopedcow <188090362+mopedcow@users.noreply.github.com>
2025-08-08 16:54:00 +0100

    Complete working console game (steps 2-3)

--- script.js
+++ script.js
@@ -13,25 +13,21 @@ function createPlayer (name, marker) {
     return { name, marker, getWins, incWins, clearWins };
 }
 
-/* Wrap the below in 'gameplay' function? */
-
 function playGame () {
-   const gameboard = newBoard();
+    const gameboard = newBoard();
+    let turnCount = 1;
+
+    const isOccupied = (row, col) => {
+        if (gameboard[row][col] !== 'n') {
+            return true;
+        } else {
+            return false;
+        }
+    }
     const placeMarker = (marker, row, col) => {
-        row = `row${row}`;
-        if (gameboard[row][col] === 'x' ||
-            gameboard[row][col] === 'o') {
-                return console.log(`! Occupied space at ${row} col${col}: '${marker}' marker not placed.`);
-            } else {
-                gameboard[row].splice(col, 1, marker);
-                return console.log(`'${marker}' marker placed: ${row} col${col}`)
-            }
+        gameboard[row].splice(col, 1, marker);
     }
-    let turnCount = 0;
-    const turnCounter = () => {
-        turnCount++;
-        return console.log(`turn count is now: ${turnCount}`);
-    }
+    const turnCounter = () => { turnCount++; }
     const rotateActivePlayer = () => {
         if (activePlayer == player1) {
             return activePlayer = player2;
@@ -76,21 +72,28 @@ function playGame () {
                 return false;
             }
     }
-    const checkEndCondition = (player, marker) => {
-        if (checkForWin(marker)) {
-            player.wins += 1;
-            console.log(`${player.name} wins!!`);
-        } else if (turnCount === 9) {
-            console.log(`Stalemate :-(`);
-        } else {
-            console.log(`No winner yet, game continues.`);
-        }
-    }
+
     const playRound = (getRow, getCol) => {
+        getRow = `row${getRow}`;
+
+        if (isOccupied(getRow, getCol)) { return console.log('error, occupied space'); }
+        
         placeMarker(activePlayer.marker, getRow, getCol);
-        turnCounter();
-        checkEndCondition();
+
+        console.log(`turn ${turnCount}`);
+        console.log(gameboard);
+
+        if (checkForWin(activePlayer.marker)) {
+            activePlayer.incWins();
+            console.log(`---${activePlayer.name} wins!!---`);
+            return console.log(`Game Over`);
+        } else if (turnCount === 9) {
+            console.log(`---Stalemate: No One Wins---`);
+            return console.log(`Game Over`);
+        }
         rotateActivePlayer();
+        turnCounter();
+        
     }
 
     return {
@@ -108,20 +111,38 @@ let newgame = playGame();
 /* End 'gamplay' function */
 
 
-/* Testing below 
-
-newGameBoard();
-
-placeMarker(player2.marker, 0, 2);
-placeMarker(player2.marker, 1, 1);
-placeMarker(player2.marker, 2, 0);
-
-console.log(gameboard);
-turnCount = 8;
-checkEndCondition(player2, player2.marker);
-
-placeMarker(player1.marker, 0, 2);
-
-newGameBoard();
-console.log(gameboard);
+/* player1 wins
+newgame.playRound(1, 1);
+newgame.playRound(0, 1);
+newgame.playRound(0, 1);
+newgame.playRound(0, 0);
+newgame.playRound(1, 2);
+newgame.playRound(2, 2);
 */
+
+newgame = playGame();
+
+/* Stalemate
+newgame.playRound(0, 0);
+newgame.playRound(1, 2);
+newgame.playRound(2, 2);
+newgame.playRound(1, 1);
+newgame.playRound(1, 0);
+newgame.playRound(2, 0);
+newgame.playRound(0, 2);
+newgame.playRound(0, 2);
+newgame.playRound(0, 1);
+newgame.playRound(2, 1);
+*/
+
+newgame = playGame();
+
+/* player 2 wins */
+newgame.playRound(0, 1);
+newgame.playRound(1, 1);
+newgame.playRound(0, 0);
+newgame.playRound(0, 2);
+newgame.playRound(2, 2);
+newgame.playRound(2, 0);
+
+newgame = playGame();
